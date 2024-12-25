b4257541e00c5f367e87780772835dd9d4de60bd
SaintKai <375287330@qq.com>
2024-12-25 18:14:24 +0800

    Git IgnoreCase False；

--- ServerWebAPI/Interceptors/GlobalInterceptor.cs
+++ ServerWebAPI/Interceptors/GlobalInterceptor.cs
@@ -24,7 +24,7 @@ namespace ServerWebAPI.Interceptors
             string? actionName = routeData.Values["action"]?.ToString();
             string currentRoute = controllerName + "/" + actionName;
             //排除的路由
-            string[] passRoutesArray = { "User/Register", "User/Login", "Client/CheckUpdate", "Client/GetPkgWgt", "File/GetAllowedMimes", "WebSocket/Connect" };
+            string[] passRoutesArray = { "User/Register", "User/Login", "Client/CheckUpdate", "Client/GetPkgWgt", "File/GetAllowedMimes", "File/GetFile", "WebSocket/Connect" };
             bool isPass = passRoutesArray.Any(r => r.Equals(currentRoute, StringComparison.OrdinalIgnoreCase));
             if (isPass)
             {
